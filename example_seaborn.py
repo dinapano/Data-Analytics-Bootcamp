1d2b84f59cfbad4b57f064dbd83ffcada179c981
dinapano <151871178+dinapano@users.noreply.github.com>
2025-01-17 15:03:53 +0200

    Delete example_seaborn.py

--- example_seaborn.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-np.random.seed(24)
-data = np.random.randint(low=10, high=100, size=(7, 7))
-hm = sns.heatmap(data=data, cmap="viridis", annot=True)
-plt.show()
